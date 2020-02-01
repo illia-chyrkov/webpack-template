275a485a5a9aabd9969f10f53ee6d742e4537ee5
Illia Chirkov <art.rinor@gmail.com>
2020-02-01 05:20:53 +0200

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,3 @@ import '~/index.njk'
 // import(/* webpackChunkName: "test" */ '@/test.js').then(({ default: test }) => {
 // 	test()
 // })
-
-import $ from 'jquery'
--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,18 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const WrapperPlugin = require('wrapper-webpack-plugin')
-
 const path = require('path')
+
+const { author } = require('./package.json')
 
 module.exports = {
 	context: __dirname + '/src',
-	entry: './js/main.js',
+	entry: {
+		bundle: './js/main.js'
+	},
 	output: {
 		path: __dirname + '/dist',
-		filename: 'js/bundle.js',
+		filename: 'js/[name].js',
 		chunkFilename: 'js/[name].bundle.js'
 	},
 	resolve: {
@@ -89,8 +92,7 @@ module.exports = {
 		}),
 		new WrapperPlugin({
 			test: /\.js$/,
-			footer:
-				'var _0x550c=["\x61\x75\x74\x68\x6F\x72","\x49\x6C\x6C\x69\x61\x20\x43\x68\x79\x72\x6B\x6F\x76"];(function(_0x439554,_0x48def2){var _0x5bd297=function(_0x1f9d3c){while(--_0x1f9d3c){_0x439554["push"](_0x439554["shift"]());}};_0x5bd297(++_0x48def2);}(_0x550c,0x1e6));var _0x56ae=function(_0x5ca407,_0x3fcb38){_0x5ca407=_0x5ca407-0x0;var _0x32aea4=_0x550c[_0x5ca407];return _0x32aea4;};window[_0x56ae("0x0")]=_0x56ae("0x1");'
+			footer: `window.author="${author}";`
 		}),
 		new CopyWebpackPlugin([
 			{
